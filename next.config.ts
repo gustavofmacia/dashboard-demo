abcf189390f238b2fce3553954cc48a76c5a67e9
gustavofmacia <gustavofmacia@gmail.com>
2025-10-07 12:31:00 -0300

    chore: add eslint, prettier, next, gitignore configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" && {
+      exclude: ["error", "warn"],
+    },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+
+  logging: { fetches: { fullUrl: true } },
+
+  typedRoutes: true,
 };
 
 export default nextConfig;
