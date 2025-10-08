f4255930e25e0c18d443bcb09ac80e643f6ac164
gustavofmacia <gustavofmacia@gmail.com>
2025-10-08 12:14:04 -0300

    chore: configure DATABASE_URL dynamically and validate env variables

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,20 @@ import "dotenv/config";
 import path from "node:path";
 import type { PrismaConfig } from "prisma";
 
+if (
+  process.env.DB_USER &&
+  process.env.DB_PASSWORD &&
+  process.env.DB_HOST &&
+  process.env.DB_PORT &&
+  process.env.DB_NAME
+) {
+  process.env.DATABASE_URL = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+} else {
+  throw new Error(
+    "Faltan variables de entorno para la conexión de la base de datos."
+  );
+}
+
 export default {
   schema: path.join("prisma"), // Prisma take schema.prisma + models/* --- https://www.prisma.io/docs/orm/prisma-schema/overview/location#multi-file-prisma-schema
 } satisfies PrismaConfig;
